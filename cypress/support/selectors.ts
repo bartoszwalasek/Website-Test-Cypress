7950b8853eba4941228bd4ed22d1329978c2079e
bartosz-walasek <b.wal1990@gmail.com>
2023-11-11 16:32:11 +0100

    create selectors file

---
+++ cypress/support/selectors.ts
@@ -0,0 +1,44 @@
+export const mainPage = {
+  nav: ".shop-menu > .nav",
+};
+
+export const loginSignupPage = {
+  form: "#form",
+  signupName: '[data-qa="signup-name"]',
+  signupEmail: '[data-qa="signup-email"]',
+  signupButton: '[data-qa="signup-button"]',
+  loginEmail: '[data-qa="login-email"]',
+  loginPassword: '[data-qa="login-password"]',
+  loginButton: '[data-qa="login-button"]',
+};
+
+export const signup = {
+  mainForm: ".login-form",
+  name: '[data-qa="name"]',
+  email: '[data-qa="email"]',
+  password: '[data-qa="password"]',
+  dateOfBirth: {
+    day: '[data-qa="days"]',
+    month: '[data-qa="months"]',
+    year: '[data-qa="years"]',
+  },
+  newsletter: "#newsletter",
+  offers: "#optin",
+  firstName: '[data-qa="first_name"]',
+  lastName: '[data-qa="last_name"]',
+  company: '[data-qa="company"]',
+  address: '[data-qa="address"]',
+  address2: '[data-qa="address2"]',
+  country: '[data-qa="country"]',
+  state: '[data-qa="state"]',
+  city: '[data-qa="city"]',
+  zipcode: '[data-qa="zipcode"]',
+  mobileNumber: '[data-qa="mobile_number"]',
+  createAccountButton: '[data-qa="create-account"]',
+};
+
+export const general = {
+  accoutCreated: '[data-qa="account-created"]',
+  accoutDeleted: '[data-qa="account-deleted"]',
+  continueButton: '[data-qa="continue-button"]',
+};
